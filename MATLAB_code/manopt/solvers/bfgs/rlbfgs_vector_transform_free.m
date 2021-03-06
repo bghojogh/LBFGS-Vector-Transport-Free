d6f8a7d55f684008a4794428695e37bf5a4751cd
Benyamin Ghojogh <bghojogh@uwaterloo.ca>
2021-03-06 16:33:55 -0500

    added the pymanopt code

---
+++ MATLAB_code/manopt/solvers/bfgs/rlbfgs_vector_transform_free.m
@@ -0,0 +1,5 @@
+function [x, cost, info, options] = rlbfgs_modified(problem, x0, options)
+
+%%%%% To be worked on....
+
+end
---
+++ Python_code/pymanopt/solvers/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "ConjugateGradient",
+    "NelderMead",
+    "ParticleSwarm",
+    "SteepestDescent",
+    "TrustRegions"
+]
+
+from .conjugate_gradient import ConjugateGradient
+from .nelder_mead import NelderMead
+from .particle_swarm import ParticleSwarm
+from .steepest_descent import SteepestDescent
+from .trust_regions import TrustRegions
