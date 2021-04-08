a08b6a7e0c94afce4dd393b0974e5f03a996135f
Benyamin Ghojogh <bghojogh@uwaterloo.ca>
2021-04-08 14:22:20 -0400

    updated

---
+++ install.m
@@ -0,0 +1,22 @@
+function install_toolbox
+
+clc
+clear all
+close all
+
+fprintf('Installing the toolbox...\n');
+
+% Find required paths
+this_file = mfilename('fullpath'); % get this file's path and name (without .m)
+toolbox_root_path = fileparts(this_file);
+
+% Add function folders to search path
+fprintf('Adding MixEst folders to search path...\n');
+addpath(toolbox_root_path)
+addpath(genpath(fullfile(toolbox_root_path, 'solvers')))
+addpath(genpath(fullfile(toolbox_root_path, 'manifolds')))
+addpath(genpath(fullfile(toolbox_root_path, 'mixest')))
+addpath(genpath(fullfile(toolbox_root_path, 'thirdparty')))
+addpath(genpath(fullfile(toolbox_root_path, 'examples')))
+fprintf('[Done]\n')
+
